22f70db86cf0fc92a2016515bb538f6ef1a74b22
vymi1016 <vyacheslav.miroshnichenko@netcracker.com>
2018-01-11 21:11:49 +0200

    Initial commit

---
+++ src/main/java/com/mining/miningwebsockets/MiningWebsocketsApplication.java
@@ -0,0 +1,12 @@
+package com.mining.miningwebsockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiningWebsocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiningWebsocketsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mining/miningwebsockets/MiningWebsocketsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mining.miningwebsockets;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiningWebsocketsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
